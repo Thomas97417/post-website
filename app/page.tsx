cd1670a1b1003d05b1f366a860057d51fb91892a
TOMA <throuquette@hotmail.fr>
2024-01-10 14:15:09 +0100

    final commit

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center gap-6 p-24">
       <p className='text-2xl font-bold'>Main Page</p>
-      <Link className={buttonVariants({variant: "default"})} href='/me'>Profile</Link>
+      <Link className={buttonVariants({variant:"secondary"})} href='/me'>Profile</Link>
       <SignInButton />
     </main>
   )
